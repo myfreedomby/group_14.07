d916a437922b1aea0549c7468780cbe53bc88c2e
davojta <damarah@gmail.com>
2016-09-25 12:42:04 +0300

    add ingridients html

--- 2016_09_25_jquery_pizza/pizza.js
+++ 2016_09_25_jquery_pizza/pizza.js
@@ -8,26 +8,32 @@ Object.prototype.values = function(obj) {
 
 var INGRIDIENTS = {
     'cheese': {
+        name: 'cheese',
         weight: 50,
         calories: 50
     },
     'sauce': {
+        name: 'sauce',
         weight: 10,
         calories: 20
     },
     'mozzarela': {
+        name: 'mozzarela',
         weight: 25,
         calories: 25
     },
     'camamber': {
+        name: 'camamber',
         weight: 25,
         calories: 25
     },
     'feta': {
+        name: 'feta',
         weight: 25,
         calories: 25
     },
     'peperoni': {
+        name: 'peperoni',
         weight: 20,
         calories: 20
     }
@@ -104,8 +110,17 @@ function logEvent(e) {
     console.log(e)
 }
 
+function renderIngridients(ingridients) {
+    function renderIngridient(ingridient) {
+        return '<li>' + ingridient.name + '</li>'
+    }
+    var result = ingridients.map(renderIngridient)
+    return '<ul class="js-ingridients">' + result.join('') + '</ul>'
+}
+
 $(function() {
     var $select = $('.js-choose-recipe')
+    var $chosenRecipe = $('.js-chosen-recipe')
 
     Object.values(recipes).forEach(function(recipe) {
         var $option = $('<option value="'+ recipe.id +'">' + recipe.name + '</option>')
@@ -114,11 +129,22 @@ $(function() {
 
     $select.on('change', function() {
         var chosenValue = $select.val()
+
         var chosenRecipe = recipes[chosenValue]
+        if (chosenRecipe === undefined) {
+            $('.js-ingridients').remove()
+            $chosenRecipe.text('Ничего не выбрано')
+
+        }
         console.log(chosenRecipe)
 
         var pizza = new Pizza(chosenRecipe)
-        $('.js-chosen-recipe').text(chosenRecipe.name)
+
+        $chosenRecipe.text(chosenRecipe.name)
+        var ingridientsHtml = renderIngridients(chosenRecipe.ingridients)
+        console.log(ingridientsHtml)
+        $('.js-ingridients').remove()
+        $chosenRecipe.after(ingridientsHtml)
 
     })
 
