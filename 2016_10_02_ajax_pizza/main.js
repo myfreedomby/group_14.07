84935658f1c78eb39a15a0b480038c0b5b8ae8a4
davojta <damarah@gmail.com>
2016-10-02 12:39:10 +0300

    fetch

--- 2016_10_02_ajax_pizza/main.js
+++ 2016_10_02_ajax_pizza/main.js
@@ -49,35 +49,36 @@ function renderHtml(recipes) {
     })
 }
 
-function getRecipes(ingridients, endCallback) {
-    $.ajax({
-        dataType: "json",
-        data: 'text',
-        url: 'http://localhost:8080/2016_10_02_ajax_pizza/pizza.json',
-        success: function(data) {
+function getRecipes(ingridients) {
+    var url = 'http://localhost:8080/2016_10_02_ajax_pizza/pizza.json'
 
-            var values = getObjectValues(data)
-            values.forEach(function (value) {
-                value.ingridients = value.ingridients.map(function (ingrName) {
-                    return ingridients[ingrName]
-                })
+    var rawRecipes = fetch(url).then(function (response) {
+        return response.json()
+    })
+    var resultPromise = Promise.all(rawRecipes, ingridients).then(function (resolvedRecipes, resolvedIngridients) {
+        var values = getObjectValues(resolvedRecipes)
+        values.forEach(function (value) {
+            value.ingridients = value.ingridients.map(function (ingrName) {
+                return resolvedIngridients[ingrName]
             })
-            console.log(data)
-            var recipes = data
+        })
+        var recipes = resolvedIngridients
+        return recipes
+    })
 
-            endCallback.apply(null, [recipes])
-        }
-    });
+    return resultPromise
 }
 
 $(function () {
-    $.getJSON("ingridients.json", function (ingridients) {
-        getRecipes(ingridients, function (recipes) {
-            renderHtml(recipes)
-        })
-        console.log(ingridients)
-    })
+    var url = 'ingridients.json'
 
+    var ingridients = fetch(url).then(function(response) {
+        if(response.status === 200) return response.json();
+        else throw new Error('Something went wrong on api server!');
+    })
+    var recipes = getRecipes(ingridients).then(function (recipes) {
+        renderHtml(recipes)
+    })
 })
 
 
